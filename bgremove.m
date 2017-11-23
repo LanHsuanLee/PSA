5af8cec2cf9a9888400009b51bbf2b0d7afa7101
Renfong <windless99@gmail.com>
2017-11-23 22:24:24 +0800

    Update bgremove.m
    remove low frequency background

--- bgremove.m
+++ bgremove.m
@@ -1,9 +1,14 @@
-function bgremoved=bgremove(img)
-
+function bgremoved=bgremove(img,order)
 %% remove low frequency background
+% img: target image
+% order: the power of 2 for resize
+
+order=fix(order);
+if order<3
+    order=3;
+end
 
 [m,n]=size(img);
-bgimg=imresize(img,[8,8]);
+bgimg=imresize(img,[2^order,2^order]);
 bgimg=imresize(bgimg,[m,n]);
 bgremoved=img-bgimg;
-
